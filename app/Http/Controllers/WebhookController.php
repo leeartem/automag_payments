2d8331d760471c5f0b53c1f4f25e49114b04b3fd
Lee Artem <fcknawsm@ya.ru>
2025-01-10 03:36:07 +0300

    m

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -9,8 +9,7 @@ class WebhookController extends Controller
 {
     public function webhook(Request $request, WebhookService $webhookService)
     {
-        dd($request->all());
-        $webhookService->run($request->all());
+//        $webhookService->run($request->all());
 
         return response()->json('Ok');
     }
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 //Route::post('/webhook/eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ', [WebhookController::class, 'webhook']);
 Route::any('/webhook/eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ', [WebhookController::class, 'webhook']);
+Route::any('/test', function () {
+    echo "hello";
+});
