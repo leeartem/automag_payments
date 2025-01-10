bdce6e29f87a82234c13cd5cad0ecb918c427e55
Lee Artem <fcknawsm@ya.ru>
2025-01-10 03:15:25 +0300

    m

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\WebhookController;
 use Illuminate\Support\Facades\Route;
 
 //Route::post('/webhook/eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ', [WebhookController::class, 'webhook']);
-Route::post('/webhook/eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ', [WebhookController::class, 'webhook']);
+Route::any('/webhook/eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ', [WebhookController::class, 'webhook']);
